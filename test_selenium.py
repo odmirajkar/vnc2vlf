53004f05e6777f3331f6cf1bec7e2f9ba3864d14
root <root@ip-172-31-16-176.ap-south-1.compute.internal>
2020-07-11 18:14:22 +0000

    added video result

--- test_selenium.py
+++ test_selenium.py
@@ -4,7 +4,7 @@ from selenium.webdriver.remote.remote_connection import RemoteConnection
 import time 
 remote_hub_url='http://127.0.0.1:4444/wd/hub' 
 options = Options() 
-options.headless = True 
+#options.headless = True 
 driver =webdriver.Remote(remote_hub_url,options=options) 
 driver.get("https://en.wikipedia.org/wiki/Reddit")
 time.sleep(10)
