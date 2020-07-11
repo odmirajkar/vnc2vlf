415b722901b3393d3b2b539a52d1f63424d7c252
root <root@ip-172-31-16-176.ap-south-1.compute.internal>
2020-07-11 18:06:57 +0000

    added test.py

---
+++ test_selenium.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options 
+from selenium.webdriver.remote.remote_connection import RemoteConnection 
+import time 
+remote_hub_url='http://127.0.0.1:4444/wd/hub' 
+options = Options() 
+options.headless = True 
+driver =webdriver.Remote(remote_hub_url,options=options) 
+driver.get("https://en.wikipedia.org/wiki/Reddit")
+time.sleep(10)
